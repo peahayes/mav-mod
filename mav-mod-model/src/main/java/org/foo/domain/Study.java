8025a752af80df813d5a218e9ae0882febf1ac97
Pam Hayes <phayes@umich.edu>
2011-11-30 10:06:24 -0500

    Add a new class

---
+++ mav-mod-model/src/main/java/org/foo/domain/Study.java
@@ -0,0 +1,23 @@
+package org.foo.domain;
+
+public class Study {
+	
+	private Long id;
+	private String irbId;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getIrbId() {
+		return irbId;
+	}
+	public void setIrbId(String irbId) {
+		this.irbId = irbId;
+	}
+	
+
+}
